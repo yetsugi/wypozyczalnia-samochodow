a9f504bcc00d45dfebd09b2a8d1a96e42e196f5c
Yetsugi <yetsugi@gmail.com>
2023-06-10 22:20:55 +0200

    wip

--- src/app/views/cars/summary/summary.component.ts
+++ src/app/views/cars/summary/summary.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Car } from 'src/app/models/car';
+import { Order } from 'src/app/models/order';
 
 import { CarService } from 'src/app/services/car.service';
 import { OrderService } from 'src/app/services/order.service';
@@ -13,14 +14,27 @@ import { OrderService } from 'src/app/services/order.service';
 export class SummaryComponent implements OnInit {
   car$!: Observable<Car>;
 
+  order!: Order;
+
   constructor(
     private readonly orderService: OrderService,
     private readonly carService: CarService
   ) {}
 
   ngOnInit(): void {
-    const order = this.orderService.getOrder()!;
+    this.order = this.orderService.getOrder()!;
 
-    this.car$ = this.carService.getCarById(order.carId!);
+    this.car$ = this.carService.getCarById(this.order.carId!);
+  }
+
+  getPaymentMethodLabel(): string {
+    switch (this.order.formData!.paymentMethod) {
+      case 'transfer':
+        return 'Przelew';
+      case 'on-site':
+        return 'Przy odbiorze';
+    }
+
+    return '';
   }
 }
